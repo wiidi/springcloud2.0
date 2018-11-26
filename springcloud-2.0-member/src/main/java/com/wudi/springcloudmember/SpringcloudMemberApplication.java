c3af8a35d298337a001011d7d7663bc2a3aed882
吴迪 <625553347@qq.com>
2018-11-26 21:09:04 +0800

    构建springcloud2.0

---
+++ springcloud-2.0-member/src/main/java/com/wudi/springcloudmember/SpringcloudMemberApplication.java
@@ -0,0 +1,12 @@
+package com.wudi.springcloudmember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudMemberApplication.class, args);
+	}
+}
---
+++ springcloud-2.0-member/src/main/java/com/wudi/springcloudmember/api/Controller/MemberApiController.java
@@ -0,0 +1,25 @@
+package com.wudi.springcloudmember.api.Controller;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: 吴迪 625553347@qq.com 18040509188
+ * @CreateDate: 2018/11/21 21:49
+ * @UpdateUser:
+ * @UpdateDate: 2018/11/21 21:49
+ * @UpdateRemark:
+ * @Version: 1.0
+ * Copyright (c) 2018,武汉中地云申科技有限公司
+ * All rights reserved.
+ **/
+@RestController
+
+public class MemberApiController {
+    @RequestMapping("/getMember")
+    public String getMember(){
+        return "this is a member";
+    }
+}
